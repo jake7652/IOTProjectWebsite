528fa9614a2d44afb14bdd393f0a2a0997a5de99
jake7652 <40675301+jake7652@users.noreply.github.com>
2018-07-21 04:12:56 -0400

    Update to accept table argument

--- sqlMultReq.php
+++ sqlMultReq.php
@@ -32,9 +32,11 @@ define('DB_NAME', $contents[3]);
 $fields = array("RTCTemperature");
 $lines = 0;
 
+$table = "DataTable";
 if(isset($_POST['arguments'])) {
 $fields = $_POST['arguments'][0]; 
 $lines = $_POST['arguments'][1]; 
+$table = $_POST['arguments'][2];
 }
 
 //get connection
@@ -50,12 +52,12 @@ $query = array();
 $num_fields = sizeof($fields);
 $result = array();
 //$query[0] = sprintf();
-//$mysqli->query("SELECT * FROM ( SELECT @row := @row +1 AS rownum, RTCDataTime FROM ( SELECT @row :=0) r, DataTable ) ranked WHERE rownum % 2 = 1");
+//$mysqli->query("SELECT * FROM ( SELECT @row := @row +1 AS rownum, RTCDataTime FROM ( SELECT @row :=0) r, " . $table . " ) ranked WHERE rownum % 2 = 1");
 //print($num_lines);
 //print($num_lines);
 //print("\n");
 if($lines == 0) {
-$lines_query = $mysqli->query("SELECT COUNT(*) FROM DataTable");
+$lines_query = $mysqli->query("SELECT COUNT(*) FROM " . $table . "");
 $num_lines = mysqli_fetch_array($lines_query)[0];
 $lines_query->close();
 $line_limit = 10000;
@@ -63,14 +65,14 @@ $interval = round($num_lines/$line_limit);
 if($num_lines > $line_limit) {
 
 for($i = 0; $i < $num_fields;$i++) {
-$query[$i] = "SELECT * FROM ( SELECT @row := @row +1 AS rownum, " . $fields[$i] . " FROM ( SELECT @row :=0) r, DataTable ) ranked WHERE rownum % " . $interval . " = 1";
+$query[$i] = "SELECT * FROM ( SELECT @row := @row +1 AS rownum, " . $fields[$i] . " FROM ( SELECT @row :=0) r, " . $table . " ) ranked WHERE rownum % " . $interval . " = 1";
 $result[$i] = $mysqli->query($query[$i]);
 }
-$time_query = "SELECT * FROM ( SELECT @row := @row +1 AS rownum, RTCDataTime FROM ( SELECT @row :=0) r, DataTable ) ranked WHERE rownum % " . $interval . " = 1";
+$time_query = "SELECT * FROM ( SELECT @row := @row +1 AS rownum, RTCDataTime FROM ( SELECT @row :=0) r, " . $table . " ) ranked WHERE rownum % " . $interval . " = 1";
 
 } else {
 for($i = 0; $i < $num_fields;$i++) {
-$query[$i] = sprintf("SELECT " . $fields[$i] . " FROM DataTable");
+$query[$i] = sprintf("SELECT " . $fields[$i] . " FROM " . $table . "");
 $result[$i] = $mysqli->query($query[$i]);
 }
 $time_query = sprintf("SELECT RTCDataTime FROM DataTable");
@@ -80,10 +82,10 @@ $time_query = sprintf("SELECT RTCDataTime FROM DataTable");
 } else {
 //query to get data from the table
 for($i = 0; $i < $num_fields; $i++) {
-$query[$i] = sprintf("SELECT " . $fields[$i] . " FROM ( SELECT * FROM DataTable ORDER BY TransmissionKey DESC LIMIT " . $lines . ") sub ORDER BY TransmissionKey ASC;");
+$query[$i] = sprintf("SELECT " . $fields[$i] . " FROM ( SELECT * FROM " . $table . " ORDER BY TransmissionKey DESC LIMIT " . $lines . ") sub ORDER BY TransmissionKey ASC;");
 $result[$i] = $mysqli->query($query[$i]);
 }
-$time_query = sprintf("SELECT RTCDataTime FROM ( SELECT * FROM DataTable ORDER BY TransmissionKey DESC LIMIT " . $lines . ") sub ORDER BY TransmissionKey ASC;");
+$time_query = sprintf("SELECT RTCDataTime FROM ( SELECT * FROM " . $table . " ORDER BY TransmissionKey DESC LIMIT " . $lines . ") sub ORDER BY TransmissionKey ASC;");
 }
 
 $increment = 1;
