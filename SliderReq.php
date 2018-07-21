9c6e735220f8dbe09d744770f5f6c8dff2397097
jake7652 <40675301+jake7652@users.noreply.github.com>
2018-07-21 03:34:29 -0400

    Add code to grab settings from file+table args

--- SliderReq.php
+++ SliderReq.php
@@ -1,16 +1,38 @@
 <?php
 //setting header to json
 header('Content-Type: application/json');
+//database settings 
+$contents = Array();
+$handle = fopen("/var/www/databaseSettings", "r");
+$lineNum = 0;
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+	if($line[strlen($line)-1] == "\n") {
+        $contents[$lineNum] = substr($line,0,strlen( $line )-1);
+	} else {
+	$contents[$lineNum] = $line; 
+	}
+     $lineNum++;
+    }
+
+    fclose($handle);
+} else {
+    // error opening the file.
+} 
+
 
 //database
-define('DB_HOST', '127.0.0.1');
-define('DB_USERNAME', 'Tide');
-define('DB_PASSWORD', 'MasterWater2025');
-define('DB_NAME', 'TideGuage');
+define('DB_HOST', $contents[0]);
+define('DB_USERNAME', $contents[1]);
+define('DB_PASSWORD', $contents[2]);
+define('DB_NAME', $contents[3]);
+
 
 $time = '20180303030303';
+$table = "DataTable";
 if(isset($_POST['arguments'])) {
-$time = $_POST['arguments'][0]; 
+$time = $_POST['arguments'][0];
+$table =  $_POST['arguments'][1];
 }
 
 //get connection
@@ -22,7 +44,7 @@ if(!$mysqli){
 
 
 //request first row from the table that matches or is greater than the time argument 
-$query = sprintf("Select * FROM DataTable WHERE RTCDataTime >= " . $time . " LIMIT 1;");
+$query = sprintf("Select * FROM " . $table . " WHERE RTCDataTime >= " . $time . " LIMIT 1;");
 $result = $mysqli->query($query);
 
 //get the associative array for the result 
@@ -30,7 +52,7 @@ $res_arr = mysqli_fetch_assoc($result);
 //if the result is empty then we just request the last row of the table
 if($res_arr == null) {
 $result->close();
-$query = sprintf("SELECT * FROM ( SELECT * FROM DataTable ORDER BY RTCDataTime DESC LIMIT 1 ) sub ORDER BY TransmissionKey ASC;");
+$query = sprintf("SELECT * FROM ( SELECT * FROM " . $table . " ORDER BY RTCDataTime DESC LIMIT 1 ) sub ORDER BY TransmissionKey ASC;");
 $result = $mysqli->query($query);
 //we only have one row so just fetch the associative array for that row
 echo json_encode(mysqli_fetch_assoc($result));
