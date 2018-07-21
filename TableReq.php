30cb5d3bd0b65d3b4ffb30abf793603669d6eaf2
jake7652 <40675301+jake7652@users.noreply.github.com>
2018-07-21 03:33:28 -0400

    Add code to load settings from file

--- TableReq.php
+++ TableReq.php
@@ -2,11 +2,36 @@
 //setting header to json
 header('Content-Type: application/json');
 
+//database settings 
+$contents = Array();
+$handle = fopen("/var/www/databaseSettings", "r");
+$lineNum = 0;
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+	if($line[strlen($line)-1] == "\n") {
+        $contents[$lineNum] = substr($line,0,strlen( $line )-1);
+	} else {
+	$contents[$lineNum] = $line; 
+	}
+     $lineNum++;
+    }
+
+    fclose($handle);
+} else {
+    // error opening the file.
+} 
+
+
 //database
-define('DB_HOST', '127.0.0.1');
-define('DB_USERNAME', 'Tide');
-define('DB_PASSWORD', 'MasterWater2025');
-define('DB_NAME', 'TideGuage');
+define('DB_HOST', $contents[0]);
+define('DB_USERNAME', $contents[1]);
+define('DB_PASSWORD', $contents[2]);
+define('DB_NAME', $contents[3]);
+
+$table = "DataTable";
+if(isset($_POST['arguments'])) {
+$table =  $_POST['arguments'][0];
+}
 
 
 //get connection
@@ -19,7 +44,7 @@ if(!$mysqli){
 $lines = 1; //how many lines from the database we should request
 
 //request the last $lines rows from the database
-$query = sprintf("SELECT * FROM ( SELECT * FROM DataTable ORDER BY RTCDataTime DESC LIMIT " . $lines . ") sub ORDER BY TransmissionKey ASC;");
+$query = sprintf("SELECT * FROM ( SELECT * FROM " . $table . " ORDER BY RTCDataTime DESC LIMIT " . $lines . ") sub ORDER BY TransmissionKey ASC;");
 $result = $mysqli->query($query);
 
 //we only have one row so just fetch the associative array for that row
