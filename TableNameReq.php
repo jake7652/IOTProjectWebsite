2d6d0722ec4029812373def44b29508296d3bc73
jake7652 <40675301+jake7652@users.noreply.github.com>
2018-07-23 03:00:24 -0400

    Accomidate for offline viewing

--- TableNameReq.php
+++ TableNameReq.php
@@ -30,10 +30,10 @@ define('DB_NAME', $contents[3]);
 
 
 //get connection
-$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$mysqli = @mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if(!$mysqli){
-	die("Connection failed: " . $mysqli->error);
+	$mysqli = @mysqli_connect("localhost", DB_USERNAME, DB_PASSWORD, DB_NAME);
 }
 
 
