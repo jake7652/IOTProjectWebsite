66822cdceab9fb2cf77b298b2d0bb64f0a5a6a7b
jake7652 <40675301+jake7652@users.noreply.github.com>
2018-08-03 06:04:23 -0400

    Formatting,commenting

--- SocketDaemon/Client/main.c
+++ SocketDaemon/Client/main.c
@@ -55,41 +55,50 @@ static int exec_prog(const char **argv)
 
 int main(int argc, char const *argv[])
 {
+    //line of the sql settings file
+    char line [BUF_LEN] = "";
+    //all lines of the sql settings file
+    char file [10][BUF_LEN];
+    //table that this pi is set to write to in the SQL settings file
+    char table[BUF_LEN] = "";
+    //File to store the settings for the database
+    FILE *plist = fopen("/var/www/databaseSettings", "r");
 
-char line [BUF_LEN] = "";
-char file [10][BUF_LEN];
-char table[BUF_LEN] = "";
-//File to store the settings for the database
-FILE *plist = fopen("/var/www/databaseSettings", "r");
-
-
+    //directory of the client files
     char clientsLoc[] = "/var/www/clients/";
     int daemons = 3;
+    //all daemon related file paths
     char * daemonPaths[3] = {"/var/www/clients/commitDaemon","/var/www/clients/sensorDaemon","/var/www/clients/commands"};
-    //FILE * daemonStatusFiles[2] = {fopen("/var/www/clients/commitDaemon","r"),fopen("/var/www/clients/sensorDaemon","r")};
+    //path for the file containing the daemon commands
     char commandFilePath[] = "/var/www/clients/commands";
+    //file pointer for the daemon command file
     FILE * commandFile;
-//go through each line of the settings file
-int i = 0;
-while (fgets(line, sizeof(line), plist)) {
-    strcpy(file[i], fTrim(line));
-    strcpy(table, fTrim(line));
-    i++;
-}
-
+    //go through each line of the SQL settings file
+    int i = 0;
+    while (fgets(line, sizeof(line), plist)) {
+        strcpy(file[i], fTrim(line));
+        strcpy(table, fTrim(line));
+        i++;
+    }
+    //addresss of the socket on this client
     struct sockaddr_in address;
+    //sock status and read var
     int sock = 0, valread;
+    //address of the socket server
     struct sockaddr_in serv_addr;
+    //name of the table that this client writes to
     char *hello = file[4];
+    //buffer that the socket reads into
     char buffer[BUF_LEN] = {0};
+    //create the socket on the local machine
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
         return -1;
     }
-
+    //0 out the server address in memory
     memset(&serv_addr, '0', sizeof(serv_addr));
-
+    //set the socket family and port
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
@@ -99,99 +108,111 @@ while (fgets(line, sizeof(line), plist)) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
-
+    //connect to the socket server
     while (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("\nConnection Failed retry in one second \n");
 
     }
+    //char array and pointer to keep track of current command value
     char commandMonitor[BUF_LEN];
     char * commandMonitorPtr =strcpy(commandMonitor,"");
+    //infinite loop
     while(1) {
-
-    char lineTemp[BUF_LEN] = "";
-    char *lineTempPt = strcpy(lineTemp,"VALID,");
-    lineTempPt = strcat(lineTemp,hello);
-    lineTempPt = strcat(lineTemp, ",");
-    for(int i = 0; i < daemons; i++ ){
-    FILE * temp = fopen(daemonPaths[i],"r");
-    fgets(line, sizeof(line),temp);
-    lineTempPt = strcat(lineTemp,line);
-    lineTempPt = strcat(lineTemp, ",");
-    fflush(temp);
-    fclose(temp);
-    }
-    lineTempPt = strcat(lineTemp, "END");
-//    printf(hello);
-    send(sock , lineTempPt , strlen(lineTempPt) , 0 );
-    printf("Information sent: ");
-    printf(lineTempPt);
-    printf("\n");
-    //connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    printf("\n Breakpoint 3");
-    valread = read( sock , buffer, BUF_LEN);
-    printf("\n Breakpoint 3");
-    printf("\n Data Read In: ");
-    printf(buffer);
-    printf("\n");
-    sleep(1);
-    if(valread <= 0) {
-    printf("Server has gone away");
-    printf("\n");
-    close(sock);
-    while ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-       printf("\n Socket creation error \n");
+        //temp string for building the message that is sent to the socket server
+        char lineTemp[BUF_LEN] = "";
+        //append valid onto the start of the message string so the server can separate our message from others
+        char *lineTempPt = strcpy(lineTemp,"VALID,");
+        //append the table onto the message
+        lineTempPt = strcat(lineTemp,hello);
+        lineTempPt = strcat(lineTemp, ",");
+        //append the status of all the daemons onto the message string
+        for(int i = 0; i < daemons; i++ ){
+            FILE * temp = fopen(daemonPaths[i],"r");
+            fgets(line, sizeof(line),temp);
+            lineTempPt = strcat(lineTemp,line);
+            lineTempPt = strcat(lineTemp, ",");
+            fflush(temp);
+            fclose(temp);
+        }
+        //append a end onto the message that's sent to the socket server
+        lineTempPt = strcat(lineTemp, "END");
+        //send the message to the socket server
+        send(sock , lineTempPt , strlen(lineTempPt) , 0 );
+        //print out of the message that we sent to the remote server
+        printf("Information sent: ");
+        printf(lineTempPt);
+        printf("\n");
+        //read in new command info from the server
+        valread = read( sock , buffer, BUF_LEN);
+        //print out the command that was read in
+        printf("\n Data Read In: ");
+        printf(buffer);
+        printf("\n");
         sleep(1);
+        //if we did not read anything, then there is a error with the server
+        if(valread <= 0) {
+            //print out that we had a error
+            printf("Server has gone away");
+            printf("\n");
+            //close the socket
+            close(sock);
+            //recreate the socket and attempt to recreate every second
+            while ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                printf("\n Socket creation error \n");
+                sleep(1);
+            }
+            // attempt to convert the ip address of the server and retry every second
+            while(inet_pton(AF_INET, "68.134.4.105", &serv_addr.sin_addr)<=0)
+            {
+                printf("\nInvalid address/ Address not supported \n");
+                sleep(1);
+            }
+            //attempt to reconnect the socket to the server every second
+            while (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+            {
+                printf("\nConnection Failed retry in one second \n");
+                sleep(1);
+            }
+
+        } //end error handling conditional
+        //append terminating character onto the end of the buffer
+        buffer[valread] = '\0';
+        //check whether we have recieved a new command vs the last one
+        if(strcmp(commandMonitor,buffer)==0) {
+        } else{
+            //copy the buffer into the command value
+            strcpy(commandMonitor,"");
+            strcpy(commandMonitor,buffer);
+            //if we recieve a 6 or 7 then we have recieved a command to start one of the daemons. Starting them is done through a bash script so that a new term window is made
+            /* Contents of one of the scripts for reference
+            #!/bin/bash
+            DISPLAY=:0 x-terminal-emulator -e /var/www/daemons/SQLDaemon & disown
+            exit 0
+            */
+            //code 6 is to start the sensor daemon
+            if(strcmp(buffer,"6") == 0) {
+                char * arguments[255] = {"/var/www/daemons/sensor.sh"};
+                exec_prog(arguments);
+            } else if(strcmp(buffer,"7") == 0){
+                char * arguments[255] = {"/var/www/daemons/SQL.sh"};
+                exec_prog(arguments);
+            }
+            //print out the new command code we recieved
+            printf("Command code recieved: ");
+            printf(buffer);
+            printf("\n");
+            //print the command code out to file
+            commandFile = fopen(commandFilePath,"w");
+            fprintf(commandFile,buffer);
+            fflush(commandFile);
+            fclose(commandFile);
     }
-     // Convert IPv4 and IPv6 addresses from text to binary form
-    while(inet_pton(AF_INET, "68.134.4.105", &serv_addr.sin_addr)<=0)
-    {
-       printf("\nInvalid address/ Address not supported \n");
+        //clear out the read buffer
+        strcpy(buffer,"");
+        //wait for one second before sending/recieving stuff from server
         sleep(1);
-    }
-
-     while (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed retry in one second \n");
-        sleep(1);
-
-    }
-
-    }
-    printf(" test breakpoint \n");
-    buffer[valread] = '\0';
-    printf(" test breakpoint \n");
-    if(strcmp(commandMonitor,buffer)==0) {
-    } else{
-    strcpy(commandMonitor,"");
-    strcpy(commandMonitor,buffer);
-    if(strcmp(buffer,"6") == 0) {
-    /* Contents of one of the scripts for reference
-    #!/bin/bash
-    DISPLAY=:0 x-terminal-emulator -e /var/www/daemons/SQLDaemon & disown
-    exit 0
-*/
-        char * arguments[255] = {"/var/www/daemons/sensor.sh"};
-        exec_prog(arguments);
-    } else if(strcmp(buffer,"7") == 0){
-        char * arguments[255] = {"/var/www/daemons/SQL.sh"};
-        exec_prog(arguments);
-    }
-    printf("Command code recieved: ");
-    printf(buffer);
-    printf("\n");
-    commandFile = fopen(commandFilePath,"w");
-    fprintf(commandFile,buffer);
-    fflush(commandFile);
-    fclose(commandFile);
-    sleep(10);
-    }
-    printf(" test breakpoint two \n");
-    strcpy(buffer,"");
-    printf(" test breakpoint two \n");
-    sleep(1);
-    }
+    } //end infinite loop
 
 
     return 0;
