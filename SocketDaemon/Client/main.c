f8d9bbd0b8ecbb3d709de50400dcb7876579260a
jake7652 <40675301+jake7652@users.noreply.github.com>
2018-08-12 14:54:14 -0400

    I honestly am not sure what i did

--- SocketDaemon/Client/main.c
+++ SocketDaemon/Client/main.c
@@ -120,6 +120,11 @@ SSL_CTX* InitCTX(void)
 
 int main(int argc, char const *argv[])
 {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char timeString[30];
+    strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S %Z", &tm);
+    printf("Daemon Started On: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
     //start daemons at beginning of this in order to get rid of faulty status reporting when this daemon is running
     const char * sensorArgs[255] = {"/var/www/daemons/sensor.sh","debug"};
     const char * sqlArgs[255] = {"/var/www/daemons/SQL.sh","debug"};
