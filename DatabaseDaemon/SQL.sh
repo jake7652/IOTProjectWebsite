6408c6148bbaa201e40300a304695c159258e981
jake7652 <40675301+jake7652@users.noreply.github.com>
2018-08-30 21:26:53 -0400

    Update script to resize window

--- DatabaseDaemon/SQL.sh
+++ DatabaseDaemon/SQL.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-DISPLAY=:0 x-terminal-emulator --geometry=42x4 -e /var/www/daemons/SQLDaemon & disown
+DISPLAY=:0 x-terminal-emulator --geometry=60x5 -e /var/www/daemons/SQLDaemon & disown
 sleep 1
